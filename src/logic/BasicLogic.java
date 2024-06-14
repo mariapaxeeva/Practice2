59c5894c67a8eded3a1004723ee91a124ec68001
GitUser <mashapacseeva@gmail.com>
2024-06-15 00:21:29 +0700

    add tryToKill method for hunters and correct other methods

--- src/logic/BasicLogic.java
+++ src/logic/BasicLogic.java
@@ -65,6 +65,7 @@ public class BasicLogic {
         MainPanel.landscapeInfo.reset();
         MainPanel.statistics.reset();
         MainPanel.events.reset();
+        StatisticsLogic.reset();
         days = 1;
     }
 
@@ -123,9 +124,9 @@ public class BasicLogic {
             return true;
         }
         // Чем старше животное, тем больше вероятность, что оно умрет от старости.
-        // Вероятность рассчитывается по формуле probability = 5 * age (в годах) - 25;
+        // Вероятность рассчитывается по формуле probability = 10 * age (в годах) - 150;
         // Средняя максимальная продолжительность жизни животных в неволе 25 лет.
-        boolean decideToDie = randomBoolean(5 * (MapCoder.decodeCreatureAge(cellData) / 360) - 25);
+        boolean decideToDie = randomBoolean(10 * (MapCoder.decodeCreatureAge(cellData) / 360) - 150);
         if (decideToDie) {
             deleteCreature(y, x);
             if (isElk) {
@@ -212,12 +213,15 @@ public class BasicLogic {
         return false;
     }
 
-    // Метод реализует процесс утоления голода посредством ..................
+    // Метод реализует процесс утоления голода посредством потребления растений
+    // лосями и лосей хищниками.
     // Чем больше уровень голода, тем больше вероятность, что существо будет есть
     // Вероятность рассчитывается по формуле probability = 2 * hunger - 20;
     private static boolean tryToEat(long cellData, int y, int x) {
         Map map = MainPanel.map;
-        boolean decideToEat = randomBoolean(2 * MapCoder.decodeCreatureHunger(cellData) - 20);
+        boolean decideToEat = false;
+        if (map.getElkType(y, x) != MapCoder.ELK_TYPE_EMPTY) { decideToEat = randomBoolean(2 * MapCoder.decodeCreatureHunger(cellData) - 20); }
+        else { decideToEat = randomBoolean(3 * MapCoder.decodeCreatureHunger(cellData) - 89); }
         if (decideToEat) {
             // Если принято решение поесть и еда находится в ближайшем доступном диапазоне, то есть
             for (int yStep = -1; yStep < 2; yStep++) {
@@ -228,9 +232,15 @@ public class BasicLogic {
                         if (map.getKillerType(y, x) == MapCoder.KILLER_TYPE_PREDATOR_MALE || map.getKillerType(y, x) == MapCoder.KILLER_TYPE_PREDATOR_FEMALE) {
                             int foodTarget = map.getElkType(yTarget, xTarget);
                             if (foodTarget != MapCoder.ELK_TYPE_EMPTY) {
-                                deleteCreature(yTarget, xTarget);
-                                MainPanel.events.update(days, "Лось съеден хищником.");
-                                map.setCreatureHunger(map.getCreatureHunger(y, x) - 63, y, x);
+                                decideToEat = randomBoolean(10);
+                                if (decideToEat) {
+                                    deleteCreature(yTarget, xTarget);
+                                    MainPanel.events.update(days, "Лось съеден хищником.");
+                                    StatisticsLogic.elkDied++;
+                                    StatisticsLogic.elkDiedByPredator++;
+                                    map.setCreatureHunger(map.getCreatureHunger(y, x) - 63, y, x);
+                                }
+                                else { map.setCreatureHunger(map.getCreatureHunger(y, x) - 63, y, x); }
                                 map.setCreatureEnergy(map.getCreatureEnergy(y, x) - 1, y, x);
                                 map.setCreatureAge(map.getCreatureAge(y, x) + 1, y, x);
                                 map.setActiveFlagCreature(0, y, x);
@@ -298,20 +308,18 @@ public class BasicLogic {
     }
 
     // Метод, отвечающий за беременность существ.
-    // Лоси могут забеременеть в возрасте старше 2х лет (720 дней)
-    // в сентябре и октябре (прошло 8 или 9 месяцев).
-    // Хищники беременеют в возрасте от 4х лет (1440 дней) в июне и июле.
+    // Животные могут забеременеть в возрасте старше 1го года (360 дней)
+    // Лоси беременеют с августа по ноябрь (прошло от 7 до 11 месяцев).
+    // Хищники беременеют с апреля по август.
     private static boolean tryToGetPregnant(long cellData, int y, int x) {
         Map map = MainPanel.map;
         int month = days / 30 % 12;
         if (map.getCreaturePregnancy(y, x) == 0
+                && map.getCreatureAge(y, x) > 359
                 && ((map.getElkType(y, x) == MapCoder.ELK_TYPE_FEMALE
-                && map.getCreatureAge(y, x) > 719
-                && map.getCreaturePregnancy(y, x) == 0
-                && (month == 8 || month == 9))
+                && month > 7 && month < 11)
                 || (map.getKillerType(y, x) == MapCoder.KILLER_TYPE_PREDATOR_FEMALE
-                && map.getCreatureAge(y, x) > 1439
-                && (month == 5 || month == 6)))) {
+                && month > 3 && month < 8))) {
             for (int yStep = -1; yStep < 2; yStep++) {
                 for (int xStep = -1; xStep < 2; xStep++) {
                     int yTarget = y + yStep;
@@ -320,7 +328,7 @@ public class BasicLogic {
                             && map.getActiveFlagCreature(yTarget, xTarget) == 1
                             && (map.getElkType(y, x) == MapCoder.ELK_TYPE_FEMALE && map.getElkType(yTarget, xTarget) == MapCoder.ELK_TYPE_MALE
                             || map.getKillerType(y, x) == MapCoder.KILLER_TYPE_PREDATOR_FEMALE && map.getKillerType(yTarget, xTarget) == MapCoder.KILLER_TYPE_PREDATOR_MALE)) {
-                        boolean decideToGetPregnant = randomBoolean(30);
+                        boolean decideToGetPregnant = randomBoolean(95);
                         if (decideToGetPregnant) {
                             map.setCreaturePregnancy(1, y, x);
                             map.setCreatureEnergy(map.getCreatureEnergy(y, x) - 4, y, x);
@@ -344,7 +352,95 @@ public class BasicLogic {
     }
 
     private static boolean tryToKill(long cellData, int y, int x) {
-        
+        Map map = MainPanel.map;
+        // Если 1 апреля, рассчитать лимит добычи
+        if (days / 30 % 12 == 3 && days % 30 == 0) {
+            ParametersSetter parametersSetter = new ParametersSetter();
+            StatisticsLogic.limitOfHuntingElkForSeason = parametersSetter.setLimitOfHuntingElk();
+            StatisticsLogic.limitOfHuntingPredatorForSeason = parametersSetter.setLimitOfHuntingPredator();
+            StatisticsLogic.elkDiedByHunterForSeason = 0;
+            StatisticsLogic.predatorDiedByHunterForSeason = 0;
+        }
+        // C сентября по январь сезон охоты
+        if (days / 30 % 12 > 7 && (StatisticsLogic.elkDiedByHunterForSeason < StatisticsLogic.limitOfHuntingElkForSeason
+                || StatisticsLogic.predatorDiedByHunterForSeason < StatisticsLogic.limitOfHuntingPredatorForSeason)) {
+            boolean decideToHunt = randomBoolean(5);
+            if (decideToHunt) {
+                // Если принято решение поохотиться и добыча находится в ближайшем доступном диапазоне, то добыть
+                for (int yStep = -1; yStep < 2; yStep++) {
+                    for (int xStep = -1; xStep < 2; xStep++) {
+                        int yTarget = y + yStep;
+                        int xTarget = x + xStep;
+                        if (isCellInMapRange(yTarget, xTarget)) {
+                            if (StatisticsLogic.elkDiedByHunterForSeason < StatisticsLogic.limitOfHuntingElkForSeason) {
+                                int huntTarget = map.getElkType(yTarget, xTarget);
+                                if (huntTarget != MapCoder.ELK_TYPE_EMPTY) {
+                                    deleteCreature(yTarget, xTarget);
+                                    MainPanel.events.update(days, "Лось убит охотником.");
+                                    StatisticsLogic.elkDied++;
+                                    StatisticsLogic.elkDiedByHunter++;
+                                    StatisticsLogic.elkDiedByHunterForSeason++;
+                                    map.setActiveFlagCreature(0, y, x);
+                                    return true;
+                                }
+                            } else if (StatisticsLogic.predatorDiedByHunterForSeason < StatisticsLogic.limitOfHuntingPredatorForSeason) {
+                                int huntTarget = map.getKillerType(yTarget, xTarget);
+                                if (huntTarget == MapCoder.KILLER_TYPE_PREDATOR_MALE || huntTarget == MapCoder.KILLER_TYPE_PREDATOR_FEMALE) {
+                                    deleteCreature(yTarget, xTarget);
+                                    MainPanel.events.update(days, "Хищник убит охотником.");
+                                    StatisticsLogic.predatorDied++;
+                                    StatisticsLogic.predatorDiedByHunter++;
+                                    StatisticsLogic.predatorDiedByHunterForSeason++;
+                                    map.setActiveFlagCreature(0, y, x);
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+                // Если рядом нет добычи, то искать ближайшую ячейку с ней.
+                // По гипотенузе прямоугольного треугольника с катетами xDistance и yDistance,
+                // где xDistance и yDistance - расстояния от текущей ячейки до целевой по осям x и y.
+                int minTarget = Map.MAP_SIZE * Map.MAP_SIZE + Map.MAP_SIZE * Map.MAP_SIZE;
+                int yTarget = y;
+                int xTarget = x;
+                for (int yTemp = 0; yTemp < Map.MAP_SIZE; yTemp++) {
+                    for (int xTemp = 0; xTemp < Map.MAP_SIZE; xTemp++) {
+                        if ((map.getKillerType(yTemp, xTemp) != MapCoder.KILLER_TYPE_EMPTY
+                                && map.getKillerType(yTemp, xTemp) != MapCoder.KILLER_TYPE_HUNTER
+                                && StatisticsLogic.predatorDiedByHunterForSeason < StatisticsLogic.limitOfHuntingPredatorForSeason)
+                                || (map.getElkType(yTemp, xTemp) != 0 && StatisticsLogic.elkDiedByHunterForSeason < StatisticsLogic.limitOfHuntingElkForSeason)) {
+                            int yDistance = Math.abs(yTemp - y);
+                            int xDistance = Math.abs(xTemp - x);
+                            int minTemp = yDistance * yDistance + xDistance * xDistance;
+                            if (minTemp < minTarget) {
+                                minTarget = minTemp;
+                                yTarget = yTemp;
+                                xTarget = xTemp;
+                            }
+                        }
+                    }
+                }
+                // Пока охотник не дойдет до ячейки с добычей, он направляется к цели по кратчайшему пути
+                if (yTarget != y || xTarget != x) {
+                    int yStep = 0; // шаг, который нужно сделать
+                    int xStep = 0;
+                    int yDistance = yTarget - y; // расстояние до целевой ячейки
+                    int xDistance = xTarget - x;
+                    if (yDistance < 0) {
+                        yStep = -1;
+                    } else if (yDistance > 0) {
+                        yStep = 1;
+                    }
+                    if (xDistance < 0) {
+                        xStep = -1;
+                    } else if (xDistance > 0) {
+                        xStep = 1;
+                    }
+                    return tryToMove(cellData, y, x, yStep, xStep);
+                }
+            }
+        }
         return false;
     }
 
@@ -361,10 +457,22 @@ public class BasicLogic {
             // (xTarget, yTarget) - координаты ячейки,в которую совершается движение
             int yTarget = y + yStep;
             int xTarget = x + xStep;
+            // Исключить миграцию охотников за пределы Алтайского края
+            if (map.getKillerType(y, x) == MapCoder.KILLER_TYPE_HUNTER
+                    && map.getLandscapeType(yTarget, xTarget) == MapCoder.LANDSCAPE_TYPE_OUTSIDE) {
+                return tryToMove(cellData, y, x, 0, 0);
+            }
             if (!isCellInMapRange(yTarget, xTarget)) {
+                if (map.getElkType(y, x) != MapCoder.ELK_TYPE_EMPTY) {
+                    StatisticsLogic.elkMigrated++;
+                    MainPanel.events.update(days, "Лось мигрировал.");
+                }
+                else if (map.getKillerType(y, x) == MapCoder.KILLER_TYPE_PREDATOR_MALE
+                    || map.getKillerType(y, x) == MapCoder.KILLER_TYPE_PREDATOR_FEMALE) {
+                    StatisticsLogic.predatorMigrated++;
+                    MainPanel.events.update(days, "Хищник мигрировал.");
+                }
                 deleteCreature(y, x);
-                StatisticsLogic.elkMigrated++;
-                MainPanel.events.update(days, "Лось мигрировал.");
                 return true;
             }
             // Если в целевой ячейке никого нет, то переместить в нее.
@@ -372,14 +480,18 @@ public class BasicLogic {
             if (map.getElkType(yTarget, xTarget) == 0 && map.getKillerType(yTarget, xTarget) == 0) {
                 moveCreature(y, x, yTarget, xTarget);
                 int landscapeTarget = map.getLandscapeType(yTarget, xTarget);
-                if (landscapeTarget == MapCoder.LANDSCAPE_TYPE_WATER || landscapeTarget == MapCoder.LANDSCAPE_TYPE_MOUNTAIN) {
-                    map.setCreatureEnergy(map.getCreatureEnergy(yTarget, xTarget) - 3, yTarget, xTarget);
-                    map.setCreatureHunger(map.getCreatureHunger(yTarget, xTarget) + 3, yTarget, xTarget);
+                if (map.getElkType(y, x) != MapCoder.ELK_TYPE_EMPTY) {
+                    if (landscapeTarget == MapCoder.LANDSCAPE_TYPE_WATER || landscapeTarget == MapCoder.LANDSCAPE_TYPE_MOUNTAIN) {
+                        map.setCreatureEnergy(map.getCreatureEnergy(yTarget, xTarget) - 3, yTarget, xTarget);
+                        map.setCreatureHunger(map.getCreatureHunger(yTarget, xTarget) + 3, yTarget, xTarget);
+                    } else {
+                        map.setCreatureEnergy(map.getCreatureEnergy(yTarget, xTarget) - 2, yTarget, xTarget);
+                        map.setCreatureHunger(map.getCreatureHunger(yTarget, xTarget) + 2, yTarget, xTarget);
+                    }
                 }
                 else {
-                    map.setCreatureEnergy(map.getCreatureEnergy(yTarget, xTarget) - 2, yTarget, xTarget);
-                    map.setCreatureHunger(map.getCreatureHunger(yTarget, xTarget) + 2, yTarget, xTarget);
-                }
+                    map.setCreatureEnergy(map.getCreatureEnergy(yTarget, xTarget) - 5, yTarget, xTarget);
+                    map.setCreatureHunger(map.getCreatureHunger(yTarget, xTarget) + 1, yTarget, xTarget);}
                 map.setCreatureAge(map.getCreatureAge(yTarget, xTarget) + 1, yTarget, xTarget);
                 map.setActiveFlagCreature(0, yTarget, xTarget);
                 return true;
